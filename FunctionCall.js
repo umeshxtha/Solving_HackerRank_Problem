c59a64ccf4fae84709ad52d3e0917dc453a04b78
umeshxtha <umesh.xtha123@gmail.com>
2024-05-07 14:59:19 +0545

    function call added

---
+++ FunctionCall.js
@@ -0,0 +1,8 @@
+function padRow() {
+    const call = "padRow";
+    return call;
+}
+
+const call = padRow(); // Assigning the result of the padRow call to a variable named 'call'
+
+console.log(call); // Output: "padRow"
--- pyramid.js
+++ pyramid.js
@@ -1,6 +1,6 @@
 const character ="#"
 const rows =[]
 for (let i = 0 ;i<8;i++){
-rows.push(character.repeat(i))
+rows.push(character.repeat(i+1))
 console.log(rows)
 }
